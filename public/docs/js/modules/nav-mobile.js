c8aff22ed8a8edda4a7731035ff9f440febbc789
Kamil Majkrzak <kamil.majkrzak@octopus.com>
2024-02-27 21:35:21 +0100

    Added focus trap & added navigation fallback when js is disabled

--- public/docs/js/modules/nav-mobile.js
+++ public/docs/js/modules/nav-mobile.js
@@ -1,36 +1,27 @@
 // @ts-check
 
-import { qs } from "./query.js";
+import { qs, qsa } from "./query.js";
 import {
   getFocusableElement,
   trapFocusForward,
   trapReverseFocus,
 } from "./focus.js";
 
-function addMobileNav() {
-  const hamburgerIcon = qs("[data-hamburger-icon]");
-  const mobileMenu = qs("[data-mobile-menu]");
+const hamburgerIcon = qs("[data-hamburger-icon]");
+const mobileMenu = qs("[data-mobile-menu]");
+const menuList = qs(".site-nav__list");
 
+function addMobileNav() {
   hamburgerIcon.addEventListener("click", (e) => {
     e.preventDefault();
     toggleMobileMenu(hamburgerIcon, mobileMenu);
   });
 
   // Listen for escape key to close the menu
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape" && mobileMenu.classList.contains("is-active")) {
-      closeMobileMenu(mobileMenu, hamburgerIcon);
-    }
-  });
+  document.addEventListener("keydown", (e) => {});
 
-  const menuList = qs(".site-nav__list");
-  mobileMenu.addEventListener("keydown", function (e) {
-    if (mobileMenu.classList.contains("is-active")) {
-      const focusElements = getFocusableElement(mobileMenu);
-      trapReverseFocus(e, focusElements.first);
-      trapFocusForward(e, focusElements.last);
-    }
-  });
+  // Menu accessibility controls
+  document.addEventListener("keydown", handleDropdownKeyboardNavigation);
 }
 
 function toggleMobileMenu(icon, mobileMenu) {
@@ -58,6 +49,34 @@ function closeMobileMenu(mobileMenu, icon) {
 
   // Re-enable scrolling on the body
   document.body.style.overflow = "";
+}
+
+function handleDropdownKeyboardNavigation(e) {
+  // Proceed only if search dropdown is active
+  if (!mobileMenu.classList.contains("is-active")) return;
+
+  // Listen for escape key to close the menu
+  if (e.key === "Escape") {
+    closeMobileMenu(mobileMenu, hamburgerIcon);
+  }
+
+  if (e.key === "Tab") {
+    // setTimeout(() => {
+    const menuItems = qsa(".site-nav__list li");
+    const firstElement = hamburgerIcon;
+    const lastElement = menuItems[menuItems.length - 1].firstChild;
+
+    if (e.shiftKey && document.activeElement === firstElement) {
+      // Shift + Tab: Move focus to the last element if the first element is currently focused
+      e.preventDefault();
+      if (lastElement) lastElement.focus();
+    } else if (!e.shiftKey && document.activeElement === lastElement) {
+      // Tab: Move focus to the first element if the last element is currently focused
+      e.preventDefault();
+      firstElement.focus();
+    }
+    // }, 0);
+  }
 }
 
 export { addMobileNav };
