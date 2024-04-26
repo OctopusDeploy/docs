e99846414dfff1e6b9aee71317e392e393aca36b
Kamil Majkrzak <kamil.majkrzak@octopus.com>
2024-04-26 10:08:10 +0200

    Accessibility improvement

--- public/docs/js/modules/theme-switcher.js
+++ public/docs/js/modules/theme-switcher.js
@@ -1,9 +1,9 @@
 class ThemeSwitcher {
   constructor() {
-    this.toggle = document.querySelector('[data-theme-toggle]');
-    if (!this.toggle) {
-      throw new Error('Toggle switch not found!');
-    }
+    this.themeSwitcher = document.querySelector('[data-theme-toggle]');
+    this.checkbox = this.themeSwitcher.querySelector(
+      '[data-theme-toggle-checkbox]'
+    );
     this.body = document.body;
     this.init();
   }
@@ -17,22 +17,42 @@ class ThemeSwitcher {
         ? 'dark'
         : 'light');
     this.body.setAttribute('data-theme', preferredTheme);
-    this.toggle.checked = preferredTheme === 'dark';
-    this.toggle.setAttribute('aria-checked', this.toggle.checked.toString());
+    this.checkbox.checked = preferredTheme === 'dark';
+    this.checkbox.setAttribute(
+      'aria-checked',
+      this.checkbox.checked.toString()
+    );
 
     this.addEventListeners();
   }
 
   addEventListeners() {
-    this.toggle.addEventListener('click', () => this.toggleTheme());
+    this.themeSwitcher.addEventListener('click', (e) => {
+      if (e.target === this.checkbox) {
+        this.toggleTheme();
+      }
+    });
+    // Handle keydown event for keyboard accessibility
+    this.themeSwitcher.addEventListener('keydown', (e) => {
+      // Enter or Space key
+      if (e.keyCode === 13 || e.keyCode === 32) {
+        e.preventDefault();
+        this.toggleTheme();
+      }
+    });
   }
 
   toggleTheme() {
     const newTheme =
       this.body.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+    // Toggle the checked state of the checkbox - keyboard fallback
+    this.checkbox.checked = newTheme === 'dark';
     this.body.setAttribute('data-theme', newTheme);
     localStorage.setItem('theme', newTheme);
-    this.toggle.setAttribute('aria-checked', this.toggle.checked.toString());
+    this.checkbox.setAttribute(
+      'aria-checked',
+      this.checkbox.checked.toString()
+    );
   }
 }
 
