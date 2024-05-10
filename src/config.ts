fc5a3aae64f96e799de455eb3c2c5f840e61c160
Kamil Majkrzak <kamil.majkrzak@octopus.com>
2024-05-10 15:40:32 +0200

    Font awesome update and article header adjsutment

--- src/config.ts
+++ src/config.ts
@@ -59,6 +59,9 @@ export const OPEN_GRAPH = {
 export const HEADER_SCRIPTS = `
 	<link rel="preload" href="/docs/css/roboto-regular.woff2" as="font" type="font/woff2" crossorigin>
 	<link rel="preload" href="/docs/css/roboto-bold.woff2" as="font" type="font/woff2" crossorigin>
+	<link href="/docs/css/fa/css/fontawesome.css" rel="stylesheet">
+	<link href="/docs/css/fa/css/brands.css" rel="stylesheet">
+	<link href="/docs/css/fa/css/solid.css" rel="stylesheet">
 	<meta name="google-site-verification" content="nIbWsTU_ELqMnLNxIexH_s6ch3m-s_MaFnl5u8WoaRM" />
 
 	<!-- Inline Script to set the initial theme -->
